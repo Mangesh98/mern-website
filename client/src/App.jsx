c116f47c2eeeff9facbcaab1f4c639d9f756c7bb
Mangesh98 <mangeshraje55555@gmail.com>
2024-01-11 16:13:46 +0530

    Navbar Created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,17 +5,19 @@ import { Service } from "./pages/Service";
 import { Register } from "./pages/Register";
 import { Contact } from "./pages/Contact";
 import { Login } from "./pages/Login";
+import { Navbar } from "./components/Navbar";
 
 const App = () => {
 	return (
 		<>
 			<BrowserRouter>
+				<Navbar />
 				<Routes>
 					<Route path="/" element={<Home />} />
 					<Route path="/about" element={<About />} />
 					<Route path="/service" element={<Service />} />
-					<Route path="/register" element={<Register />} />
 					<Route path="/contact" element={<Contact />} />
+					<Route path="/register" element={<Register />} />
 					<Route path="/login" element={<Login />} />
 				</Routes>
 			</BrowserRouter>
