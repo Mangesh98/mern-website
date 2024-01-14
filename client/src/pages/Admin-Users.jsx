68906123c1b2bddb3af5ccc4af94f46664c8ec18
Mangesh98 <mangeshraje55555@gmail.com>
2024-01-14 15:20:12 +0530

    Admin User page Created

--- client/src/pages/Admin-Users.jsx
+++ client/src/pages/Admin-Users.jsx
@@ -1,3 +1,66 @@
+import { useEffect, useState } from "react";
+import { useAuth } from "../store/auth";
+
 export const AdminUsers = () => {
-	return <h1>Admin Users</h1>;
+	const [users, setUsers] = useState([]);
+
+	const { authorizationToken } = useAuth();
+	const URL = "http://localhost:5000/api/admin/users";
+	const getAllUsersData = async () => {
+		try {
+			const response = await fetch(URL, {
+				method: "GET",
+				headers: {
+					Authorization: authorizationToken,
+				},
+			});
+			// console.log(authorizationToken);
+			if (response.ok) {
+				const data = await response.json();
+				setUsers(data);
+				// console.log(data);
+			}
+		} catch (error) {
+			console.log(error);
+		}
+	};
+	useEffect(() => {
+		getAllUsersData();
+	}, []);
+
+	return (
+		<>
+			<section className="admin-users-section">
+				<div className="container">
+					<h1>Admin Users Data</h1>
+				</div>
+			</section>
+			<div className="container admin-users">
+				<table>
+					<thead>
+						<tr>
+							<th>Name</th>
+							<th>Email</th>
+							<th>Phone</th>
+							<th>Update</th>
+							<th>Delete</th>
+						</tr>
+					</thead>
+					<tbody>
+						{users.map((curUser, index) => {
+							return (
+								<tr key={index}>
+									<td>{curUser.email}</td>
+									<td>{curUser.phone}</td>
+									<td>{curUser.username}</td>
+									<td>Edit</td>
+									<td>Delete</td>
+								</tr>
+							);
+						})}
+					</tbody>
+				</table>
+			</div>
+		</>
+	);
 };
--- client/src/store/auth.jsx
+++ client/src/store/auth.jsx
@@ -7,6 +7,7 @@ export const AuthProvider = ({ children }) => {
 	const [token, setToken] = useState(localStorage.getItem("token"));
 	const [user, setUser] = useState("");
 	const [services, setServices] = useState("");
+	const authorizationToken = `Bearer ${token}`;
 
 	//function to stored the token in local storage
 	const storeTokenInLS = (serverToken) => {
@@ -32,7 +33,7 @@ export const AuthProvider = ({ children }) => {
 			const response = await fetch("http://localhost:5000/api/auth/user", {
 				method: "GET",
 				headers: {
-					Authorization: `Bearer ${token}`,
+					Authorization: authorizationToken,
 				},
 			});
 
@@ -73,7 +74,14 @@ export const AuthProvider = ({ children }) => {
 
 	return (
 		<AuthContext.Provider
-			value={{ isLoggedIn, storeTokenInLS, LogoutUser, user, services }}
+			value={{
+				isLoggedIn,
+				storeTokenInLS,
+				LogoutUser,
+				user,
+				services,
+				authorizationToken,
+			}}
 		>
 			{children}
 		</AuthContext.Provider>
